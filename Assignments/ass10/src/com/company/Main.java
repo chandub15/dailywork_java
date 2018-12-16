7312c2e20b8c932de245f82dbe0f4751e0b4485c
Chandra Prakash <chandraprakashb15@gmail.com>
2018-12-16 13:01:38 +0530

    java assn

---
+++ Assignments/ass10/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner sc=new Scanner(System.in);
+
+        String s=sc.next();
+        int n=sc.nextInt();
+        int len=s.length();
+        char[] ch=s.toCharArray();
+        System.out.print(s);
+        int ite=n;
+        while(ite!=0) {
+            for (int i = (len - n); i < len; i++) {
+                System.out.print(ch[i]);
+            }
+            ite--;
+        }
+    }
+}
